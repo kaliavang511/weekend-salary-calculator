295abbdf47280d2081950b71b1d6d13dbfeb845a
Kalia Vang <kaliavang511@gmail.com>
2024-05-06 12:50:56 -0500

    fixed footer& monthly total

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
+
+let totalMonthlyValue = 0
    function handleSubmit(event) {
-    // console.log("handleSubmit works...")
     event.preventDefault()
 
-    // function validate(){
-    //   var text1 = document.querySelector('input').value;
-      
-    //   if (text1 == ''){
-    //     alert("Must fill everything in");
-    //     return false;
-    //   }
-     
-    //   }
-
-    //   validate()
-     
     let firstNameInput = document.getElementById("firstName").value 
     let lastNameInput = document.getElementById("lastName").value 
     let idInput = document.getElementById("id").value 
     let titleInput = document.getElementById("title").value 
     let annualSalaryInput = document.getElementById("annualSalary").value 
-
-   
-
 
     let firstNameList = document.getElementById('list')
      firstNameList.innerHTML += `
@@ -35,66 +21,43 @@
 
       </tr>`
 
-      var table = document.getElementById("table"), sum = 0;
-  // getting the tableID
-  function getSum(){
-    for(let i = 1; i < table.rows.length; i++){
-      //looping through the table rows
-           sum = sum + parseFloat(table.rows[i].cells[4].innerHTML);
-           //adding cell 4's row
-    }
-         
-
-  }
   
-  getSum()
 
-  function getMonthy (){
-   let monthly = sum/ 12
-   document.getElementById("totalMonthly").innerHTML = "$  "+monthly.toFixed(2);
-     
-   
+    function renderTotalMonthly() {
+    console.log("Current Total Monthly = ", totalMonthlyValue)
+    let totalMonthlySpan = document.getElementById("totalMonthlyValue")
+    totalMonthlySpan.innerHTML = totalMonthlyValue
   }
 
-  getMonthy()
-
-
-  function exceedLimit(){
-    let monthly = sum/ 12
-    if (monthly >= 20000){
-    document.getElementById("exceedLimit").className += 'over-budget'
-    }
-  }
-  exceedLimit()
+  renderTotalMonthly()
   
- }
+  document.getElementById("form").reset()
+
+  totalMonthlyValue += annualSalaryInput /12
+  renderTotalMonthly()
+  
+
+
+  let footer = document.querySelector("footer")
+  if(totalMonthlyValue >20000){
+      footer.classList.add('over-budget')
+  }
+    
+}
+
+  
+    
+
 
  function deleteThis(event){
    let task = event.target.parentElement.parentElement;
    task.remove();
- }  
+ } 
+
+
+
 
  
 
-function onClickRemove(deleteButton) {
-  let row = deleteButton.parentElement.parentElement;
-  row.parentNode.removeChild(row);
-  updateSubTotal();
-}
-
-
-
-  const formToReset = document.getElementById('form');
-  //grabbing the form id 
-
-  formToReset.addEventListener('submit', (e) => {
-  //when submit and event is gonna happen    
-     e.preventDefault();
-     //need to prevent it, in order to render it right 
-     formToReset.reset();
-     //resetting all inputs 
-
-     
-  });
 
   
